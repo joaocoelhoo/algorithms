7070616542eb0b548a6d8cd36819e669115004c5
Joao Coelho <joaopaulomelocoelho@gmail.com>
2022-11-23 16:04:11 -0300

    create cryptography test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,19 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    message = "CUSCUZ"
+
+    first_case = "SUC_ZUC"
+    second_case = "ZU_CUSC"
+    third_case = "ZUCSUC"
+
+    assert encrypt_message(message, 3) == first_case
+    assert encrypt_message(message, 4) == second_case
+    assert encrypt_message(message, -1) == third_case
+
+    with raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message, "doritos")
+    with raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(10, 4)
