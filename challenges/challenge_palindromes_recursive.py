25fcd9656f5988aae5f018237dd7e4911ede1153
Joao Coelho <joaopaulomelocoelho@gmail.com>
2022-11-27 11:16:22 -0300

    create palindrome function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    if len(word) <= 2 and word[low_index] == word[high_index]:
+        return True
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    return is_palindrome_recursive(word[1:-1], 0, -1)
