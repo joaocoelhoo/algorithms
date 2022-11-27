3203cbcb574ca8a7054a4c79ee3d21d396f24b04
Joao Coelho <joaopaulomelocoelho@gmail.com>
2022-11-27 12:14:56 -0300

    create anagram function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,24 @@
+def selection_sort(array):
+    for i in range(len(array)):
+        minimum = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[minimum]:
+                minimum = j
+        array[minimum], array[i] = array[i], array[minimum]
+    return array
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(first_string) == 0 and len(second_string) == 0:
+        return (first_string, second_string, False)
+
+    first_string_lowercase = first_string.lower()
+    second_string_lowercase = second_string.lower()
+
+    first_list_sorted = selection_sort(list(first_string_lowercase))
+    second_list_sorted = selection_sort(list(second_string_lowercase))
+    first_string_sorted = "".join(first_list_sorted)
+    second_string_sorted = "".join(second_list_sorted)
+    anagram = first_string_sorted == second_string_sorted
+
+    return (first_string_sorted, second_string_sorted, anagram)
